4f43f1cfcd532861f379f7fbfa1dcfcdc2f8b526
kuznet.i.sova <kis7425910@gmail.com>
2020-05-19 23:56:41 +0300

    Initial Commit

---
+++ TrafficLightApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficLightApp
+//
+//  Created by Ирина Кузнецова on 19.05.2020.
+//  Copyright © 2020 Irina Kuznetsova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
