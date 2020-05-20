d2c8d9c58e9fb5d5b7d04200e742944807f0766b
kuznet.i.sova <kis7425910@gmail.com>
2020-05-20 23:17:12 +0300

    added outlets and button

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        redView.layer.cornerRadius = 65
+        yellowView.layer.cornerRadius = 65
+        greenView.layer.cornerRadius = 65
+        startButton.layer.cornerRadius = 20
     }
 
-
+    @IBAction func startButtonPressed() {
+//        if startButton.currentTitle == "START" {
+//            redView.alpha = 1
+//            startButton.setTitle("NEXT", for: .normal)
+//        }
+        print("startButtonPressed")
+    }
 }
 
