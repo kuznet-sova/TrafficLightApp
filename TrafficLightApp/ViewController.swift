54853034ec65abaf260885aa6fc939d507c27e6a
kuznet.i.sova <kis7425910@gmail.com>
2020-05-21 00:39:55 +0300

    added switching logic

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var redView: UIView!
-    @IBOutlet weak var yellowView: UIView!
-    @IBOutlet weak var greenView: UIView!
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
     @IBOutlet var startButton: UIButton!
     
     override func viewDidLoad() {
@@ -24,11 +24,18 @@ class ViewController: UIViewController {
     }
 
     @IBAction func startButtonPressed() {
-//        if startButton.currentTitle == "START" {
-//            redView.alpha = 1
-//            startButton.setTitle("NEXT", for: .normal)
-//        }
-        print("startButtonPressed")
+        if startButton.currentTitle == " START " {
+            redView.alpha = 1
+            startButton.setTitle(" NEXT ", for: .normal)
+        } else if redView.alpha == 1 {
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+        } else if yellowView.alpha == 1 {
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+        } else {
+            greenView.alpha = 0.3
+            redView.alpha = 1
+        }
     }
 }
-
